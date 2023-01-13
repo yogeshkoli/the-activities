83a9fa934b2369c6436e75835ab6dbd7cf6f1da2
Yogesh Koli <yogeshkoli@hotmail.com>
2023-01-13 10:18:31 +0530

    Declared Mediator in BaseAPI Class  - we are going to need _mediator in every class where Query required  - instead of declaring it in each class simply move up into base class

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,22 +1,14 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-             return await _mediator.Send(new List.Query());
+             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
